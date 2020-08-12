bf26ae29d6524899e5c32ff7bae80066e761f7c3
Gabriel Morillo <gbrlmrllo@gmail.com>
2020-08-12 17:06:46 -0300

    Add ember-cli-content-security-policy

---
+++ config/content-security-policy.js
@@ -0,0 +1,25 @@
+// config/content-security-policy.js
+
+module.exports = function() {
+  return {
+    delivery: ['meta'],
+    policy: {
+      // Deny everything by default
+      'default-src': ["'none'"],
+      // Allow scripts
+      'script-src':  ["'self'"],
+      // Allow fonts
+      'font-src': ["'self'"],
+      // Allow data (xhr/websocket)
+      'connect-src': ["'self'"],
+      // Allow images from the origin itself (i.e. current domain)
+      'img-src': ["'self'"],
+      // Allow CSS
+      'style-src': ["'self'"],
+      // Omit `media-src` from policy
+      // Browser will fallback to default-src for media resources (which is 'none', see above)
+      'media-src': null
+    },
+    reportOnly: false
+  };
+};
