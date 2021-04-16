0fbb3036a3cf6d7865b69039d3a150e93210e867
Gabriel Morillo <gbrlmrllo@gmail.com>
2021-04-16 15:02:52 -0300

    Upgrade ember to V3.26.0 (#5)
    - Add macro decorators

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,8 +1,14 @@
-import Application from '../app';
+import Application from 'ember-boilerplate/app';
 import config from 'ember-boilerplate/config/environment';
+import * as QUnit from 'qunit';
 import { setApplication } from '@ember/test-helpers';
+import { setup } from 'qunit-dom';
 import { start } from 'ember-qunit';
 
+setup(QUnit.assert);
+
 setApplication(Application.create(config.APP));
+
+setup(QUnit.assert);
 
 start();
